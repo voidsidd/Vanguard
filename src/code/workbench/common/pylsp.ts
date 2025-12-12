5c38f48883c98828e3937fa661c870d519d2e657
Siddarth <sidzy4@gmail.com>
2025-12-12 21:25:32 +0530

    chore: pylsp install on venv

--- src/code/workbench/common/pylsp.ts
+++ src/code/workbench/common/pylsp.ts
@@ -1,25 +1,50 @@
-import { _extensions } from "../../platform/extension/common/extension";
-import { showDownloadBox } from "../../platform/messagebox/common/messagebox";
+import { _extensions } from "../../platform/extension/common/extension.js";
+import { showDownloadBox } from "../../platform/messagebox/common/messagebox.js";
+import { IProjectDetails } from "../workbench.types.js";
 
 async function check() {
-  const result = await window.ipc.invoke(
-    "workbench.workspace.python.check.package",
-    "python-lsp-server"
-  );
-
-  const arg =
-    window.node.platform === "linux" ? "--break-system-packages" : "-v";
-
-  if (!result) {
-    await showDownloadBox(
-      "Install Python Language Server",
-      "python-lsp-server is required for Python IntelliSense, linting, and formatting features. Install it now to enable full Python support?",
-      "python",
-      ["-m", "pip", "install", "python-lsp-server[all]", arg],
-      () => {
-        window.workbench.reload();
-      }
+  const project_details = (await window.ipc.invoke(
+    "workbench.workspace.details"
+  )) as IProjectDetails;
+  if (project_details?.venv?.python) {
+    const result = await window.ipc.invoke(
+      "workbench.workspace.python.project.check.package",
+      "python-lsp-server"
     );
+
+    console.log(project_details);
+
+    if (!result) {
+      showDownloadBox(
+        "Install Python Language Server",
+        "python-lsp-server is required for Python IntelliSense, linting, and formatting features. Install it now to enable full Python support?",
+        project_details.venv.python,
+        ["-m", "pip", "install", "python-lsp-server[all]"],
+        () => {
+          window.workbench.reload();
+        }
+      );
+    }
+  } else {
+    const result = await window.ipc.invoke(
+      "workbench.workspace.python.check.package",
+      "python-lsp-server"
+    );
+
+    const arg =
+      window.node.platform === "linux" ? "--break-system-packages" : "-v";
+
+    if (!result) {
+      showDownloadBox(
+        "Install Python Language Server",
+        "python-lsp-server is required for Python IntelliSense, linting, and formatting features. Install it now to enable full Python support?",
+        "python",
+        ["-m", "pip", "install", "python-lsp-server[all]", arg],
+        () => {
+          window.workbench.reload();
+        }
+      );
+    }
   }
 }
 
