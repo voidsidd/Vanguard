44ad5cb2c5fbc9e2e322e04d18176a782ad417f7
Siddarth <sidzy4@gmail.com>
2026-02-28 11:10:02 +0530

    chore(splitter): update gutter size

--- src/services/layouts/layout.renderer.ts
+++ src/services/layouts/layout.renderer.ts
@@ -138,7 +138,7 @@ function renderNode(opts: {
 
     const splitter = Splitter({
       direction: dir,
-      gutterSize: dir === "vertical" ? 9 : 7,
+      gutterSize: dir === "vertical" ? 8 : 7,
       panels: renderedChildren.map(({ result }, idx) => ({
         id: String(renderedChildren[idx].i),
         size: normSizes[idx],
